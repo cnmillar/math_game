8304262f69878a3efd824288f3e7b8ece3993954
cnmillar <cnmillar@gmail.com>
2015-05-30 01:21:52 +0000

    added the better math optional exercise

--- one_game.rb
+++ one_game.rb
@@ -3,11 +3,20 @@ require 'pry'
 require 'colorize'
 
 def one_game(player)
+		math_type = ["plus", "minus", "multiplied by"].sample
 		num1 = rand(1...20)
 		num2 = rand(1...20)
-		real_answer= num1 + num2
+		case 
+		when math_type == "plus"
+			real_answer = num1 + num2
+		when math_type == "minus"
+			real_answer = num1 - num2
+		when math_type == "multiplied"
+			real_answer = num1 * num2
+		end
+		
 
-		puts "#{player.name.capitalize}: What does #{num1} plus #{num2} equal?"#.colorize(player.color.is_sym)
+		puts "#{player.name.capitalize}: What does #{num1} #{math_type} #{num2} equal?"#.colorize(player.color.is_sym)
 		player_answer = gets.chomp
 
 		player.lose_life if (real_answer != player_answer.to_i)
